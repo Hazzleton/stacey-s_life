c75f49967b1c0834537af2bc5ba52f952e9381d0
Hazzy <willis.harriet@icloud.com>
2018-12-04 18:40:26 +0000

    bit of tidying
    the background img on about no longer affects the whole page
    added some javascript so you can see how that works.
    Still have no idea whats been changed to make the nav bar underline on some pages...

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  $(".asiaword").hide();
+  $("#pond").mouseenter(function(){
+      $("#pondword").show();
+  });
+
+  $("#pond").mouseleave(function(){
+      $("#pondword").hide();
+  });
+  $("#tokyo").mouseenter(function(){
+      $("#tokyoword").show();
+  });
+
+  $("#tokyo").mouseleave(function(){
+      $("#tokyoword").hide();
+  });
+
+});
